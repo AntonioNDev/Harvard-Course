f4d2f23cb36019a7f22101276f357bf9a523fb74
AntonioNDev <antonionikoloski2003@gmail.com>
2022-01-13 03:32:34 +0100

    NewYear changes

--- Django/newyear/views.py
+++ Django/newyear/views.py
@@ -1,11 +1,18 @@
+from calendar import month
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
 # Create your views here.
 
+
+
+
+
 def index(request):
    now = datetime.datetime.now()
+   dateTimeNow = datetime.datetime.now().strftime("%d/%m/%Y")
    return render(request, "newyear/index.html", {
-      "newyear": now.month == 1 and now.day == 1
+      "newyear": now.month == 1 and now.day == 1,
+      "dateNow": dateTimeNow
 })
